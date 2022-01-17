31f176284e82f920b17187e1c0553bef05433743
Flávio Valença <flaviovalenca@gmail.com>
2022-01-16 22:29:07 -0300

    RECT_APP_BACKEND_URL

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.RECT_APP_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = process.env.RECT_APP_BACKEND_URL ?? "https://flaviovalencadsmovie.herokuapp.com";
